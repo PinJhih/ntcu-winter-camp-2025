904629b33e7bff169b280031e4d4b3e3dfceb48b
PinJhih <isaacwang29@gmail.com>
2025-02-08 15:26:20 +0800

    Complete ai chat bot

--- 03-ai-bot.py
+++ 03-ai-bot.py
@@ -9,9 +9,34 @@ TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 # ================================================== #
-# TODO： 加入 Gemini 模型的程式碼、並使用 API_KEY 連線    #
+#  TODO： 加入 Gemini 模型的程式碼、並使用 API_KEY 連線   #
 # ================================================== #
+# =================== 參考解答 ====================== #
 
+import google.generativeai as genai
+
+genai.configure(api_key=API_KEY)
+
+# 設定模型參數
+model_name = "gemini-1.5-flash"
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 256,
+    "response_mime_type": "text/plain",
+}
+
+# 建立模型
+model = genai.GenerativeModel(
+    model_name=model_name,
+    generation_config=generation_config,
+)
+
+# 開始聊天
+chat_session = model.start_chat(history=[])
+
+# ================================================== #
 
 # 建立聊天機器人
 intents = discord.Intents.default()
@@ -41,15 +66,17 @@ async def on_message(message):
     if not client.user.mentioned_in(message):
         return # 忽略這個訊息
 
-    # ================================================== #
+    # ==================================================== #
     # TODO: 修改這裡的程式碼 讓 response 是 Gemini 產出的文字 #
-    # 也可以加入你的創意，讓聊天機器人有更多功能                #
-    # ================================================== #
-    response = "Hello!"
+    # 也可以加入你的創意，讓聊天機器人有更多功能               #
+    # ==================================================== #
+    # =================== 參考解答 ======================== #
+    response = chat_session.send_message(content)
 
     # 傳送訊息給頻道
-    await message.channel.send(response)
+    await message.channel.send(response.text)
 
+    # ================================================== #
 
 # 啟動聊天機器人
 client.run(TOKEN)
